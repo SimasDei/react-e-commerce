2f3448f4266d96e5925344b3c6f550ac9bd2d20b
Simas <simonas.deimantavicius@mediapark.com>
2019-11-07 14:14:30 +0200

    add homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.style.scss';
+
+const HomePage  = () => (
+  <div className="homepage">
+    <div className="direcotry-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shopw now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jackets</h1>
+          <span className="subtitle">Shopw now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Sneakers</h1>
+          <span className="subtitle">Shopw now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Womens</h1>
+          <span className="subtitle">Shopw now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Mens</h1>
+          <span className="subtitle">Shopw now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
