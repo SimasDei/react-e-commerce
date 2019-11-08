fc9accb8876d73f6e43ce52b3b61a0137180c274
Simas <simonas.deimantavicius@mediapark.com>
2019-11-08 12:24:45 +0200

    add menu item styling

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,9 +4,10 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div className={`menu-item ${size}`} style={{backgroundImage: `url(${imageUrl})`}}>
+    <div className={`menu-item ${size}`} >
+    <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className="content">
-          <h1 className="title">{title}</h1>
+          <h1 className="title">{title.toUpperCase()}</h1>
           <span className="subtitle">Shopw now</span>
         </div>
     </div>
