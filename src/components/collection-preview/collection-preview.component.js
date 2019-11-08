962c47bf75e7c67908265dd30455f8e8ad9cbeda
Simas <simonas.deimantavicius@mediapark.com>
2019-11-08 13:41:23 +0200

    add collection preview and item styling

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import './collection-preview.styles.scss';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 const CollectionPreview = ({title, items}) => {
   return (
     <div className="collection-preview">
@@ -10,8 +12,8 @@ const CollectionPreview = ({title, items}) => {
         {
           items
           .filter((item, index) => index < 4)
-          .map((item) => (
-            <div key={item.id} className="item">{item.name}</div>
+          .map(({id, ...otherItemProps}) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))
         }
       </div>
