aefd0bebd2eebc59e21aa79a86bf0667847cf6d1
Simas <simonas.deimantavicius@mediapark.com>
2019-11-12 13:03:31 +0200

    add prettier, refactoring code part 1/?

--- src/App.js
+++ src/App.js
@@ -6,21 +6,37 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import SignInAndSingUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import { auth } from './firebase/firebase.utils.js';
 
 import Header from './components/header/header.component';
 
+class App extends React.Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route  path="/shop" component={ShopPage} />
-        <Route  path="/signin" component={SignInAndSingUpPage} />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSingUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,53 +8,53 @@ import CustomButton from '../custom-button/custom-button.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 export class SignIn extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       email: '',
-      password: ''
-    }
+      password: '',
+    };
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   handleChange = (e) => {
-    const {value, name } = e.target;
-    this.setState({[name]: value});
-  }
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form submit={this.handleSubmit}>
-          <FormInput 
-          type="email" 
-          name="email" 
-          value={this.state.email} 
-          required={true} 
-          handleChange={this.handleChange}
-          label={'Email'}
+          <FormInput
+            type='email'
+            name='email'
+            value={this.state.email}
+            required={true}
+            handleChange={this.handleChange}
+            label={'Email'}
           />
-          <FormInput 
-          type="password" 
-          name="password" 
-          value={this.state.password} 
-          required={true} 
-          handleChange={this.handleChange}
-          label={'Password'}
+          <FormInput
+            type='password'
+            name='password'
+            value={this.state.password}
+            required={true}
+            handleChange={this.handleChange}
+            label={'Password'}
           />
-          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton type='submit'>Sign In</CustomButton>
           <CustomButton onClick={signInWithGoogle}>Sign In with Google</CustomButton>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default SignIn
+export default SignIn;
