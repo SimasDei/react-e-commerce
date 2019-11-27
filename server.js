46804b2933f92bb5eb009f4c6adbbd9056a2c938
Simas <simonas.deimantavicius@mediapark.com>
2019-11-27 14:15:55 +0200

    setup basic express server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.joing(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw new Error(error);
+  console.log(`server runing on http://localhost:${port} 🚢 o/`);
+});
