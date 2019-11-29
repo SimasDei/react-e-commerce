c739f18ae6ee1f130e283895537b5b7cc57fec0b
SimasDei <simondeiman@gmail.com>
2019-11-29 18:35:52 +0200

    add graphQl query for individual collections

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import {
   CollectionPageContainer,
@@ -24,8 +21,4 @@ const CollectionPage = ({ collection: { title, items } }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
